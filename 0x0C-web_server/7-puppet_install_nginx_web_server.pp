e3435238dbd7f2c4e2530112341a51dd8484096e
alzheimeer <fogniebla@hotmail.com>
2019-12-12 18:36:52 -0500

    fix task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,6 +5,4 @@ exec {'/usr/bin/env apt-get -y update': }
 exec {'/usr/bin/env apt-get -y install nginx': }
 exec {'/usr/bin/env echo "Holberton School" > /var/www/html/index.nginx-debian.html': }
 exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
 exec {'/usr/bin/env service nginx start': }
