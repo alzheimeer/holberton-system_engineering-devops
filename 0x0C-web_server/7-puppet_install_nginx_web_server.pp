eba84e36c81d897f7f6d779a01d7b4120d7dc9db
alzheimeer <fogniebla@hotmail.com>
2019-12-12 18:44:42 -0500

    fix task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,6 +2,6 @@
 #Install nginx
 
 exec {'Install nginx':
-  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo "Holberton School" > /var/www/html/index.nginx-debian.html && new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "42i $new_string" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get -y update && sudo apt-get -y install nginx && sudo echo "Holberton School" > /var/www/html/index.html && new_string="\\\trewrite ^/redirect_me https://www.youtube.com/ permanent;" && sudo sed -i "30i $new_string" /etc/nginx/sites-available/default && sudo service nginx start',
     provider => shell,
     }
