3eb32755e05b246239a42f8e6f3a025c6ecb0903
alzheimeer <fogniebla@hotmail.com>
2020-02-03 23:04:38 -0500

    feature task 0 api y filter

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,22 +9,24 @@ if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     args = {'userId': sys.argv[1]}
     args1 = {'id': sys.argv[1]}
+    # Get todo list
     response0 = requests.get(url + 'todos/', params=args)
+    # Get user info
     response1 = requests.get(url + 'users/', params=args1)
     if response0.status_code == 200:
-        o = response0.json()
+        user = response0.json()
         a = 0
         b = 0
         c = []
-        for n in o:
+        for n in user:
             if (n.get('completed')) is True:
                 a = a+1
                 c.append(n.get('title'))
-                b = b+1
+            b = b+1
     if response1.status_code == 200:
         t = response1.json()
-        for u in t:
-            print('Employee {} is done with tasks({}/{}):'
-                  .format(u.get('name'), a, b))
-        for list in c:
-            print("\t {}".format(list))
+        print("Employee {} is done with tasks({}/{}):"
+              .format(t[0]['name'], a, b))
+        if len(c) > 0:
+            for list in c:
+                print("\t {}".format(list))
