6327121f9e8750037c18fa2b829983930314c24a
alzheimeer <fogniebla@hotmail.com>
2020-02-04 00:10:55 -0500

    fix task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,20 +7,19 @@ import sys
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/todos'
+    url = 'https://jsonplaceholder.typicode.com/'
     params = (('userId', sys.argv[1]),)
-    response0 = requests.get(url, params=params)
+    response0 = requests.get(url + 'todos/', params=params)
     if response0.status_code == 200:
         t = response0.json()
 
-    url = 'https://jsonplaceholder.typicode.com/users'
     params = (('id', sys.argv[1]),)
-    response1 = requests.get(url, params=params)
+    response1 = requests.get(url + 'users/', params=params)
     if response1.status_code == 200:
         user = response1.json()
 
     with open("{}.csv".format(sys.argv[1]), 'w', newline='') as csvfile:
-        cvs_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        cvsW = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in t:
-            cvs_writer.writerow([int(sys.argv[1]), user[0]['username'],
+            cvsW.writerow([int(sys.argv[1]), user[0]['username'],
                                  task['completed'], task['title']])
