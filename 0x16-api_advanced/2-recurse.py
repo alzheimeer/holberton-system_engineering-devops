84aff17c955c102cdb3c4d1896adc2e984369ffe
alzheimeer <fogniebla@hotmail.com>
2020-02-06 23:57:13 -0500

    bad task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-'''that queries the Reddit API and returns the number of subscribers'''
+'''returns a list containing the titles of all hot articles for a given subreddit'''
+import requests
 
 
+def recurse(subreddit, hot_list=[], after=""):
+    ''' recursive function'''
 
+    url = 'http://reddit.com/r/{}/hot.json?after={}'
+    headers = {'User-agent': 'colombiandreamm'}
+    response = requests.get(url.format(subreddit, after),
+                            headers=headers)
+    if response.status_code == 200:
+        key = response['data']['after']
+        parent = response['data']['children']
+        for obj in parent:
+            hot_list.append(obj['data']['title'])
+        if key is not None:
+            recurse(subreddit, hot_list, key)
+        return hot_list
 
-if __name__ == '__main__':
-
+    #    top10 = response.json()
+    #    for i in range(10):
+    #        print(top10['data']['children'][i]['data']['title'])
+    else:
+        return print("None")
