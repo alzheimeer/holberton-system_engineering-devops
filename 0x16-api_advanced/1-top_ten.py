8965f44639b4abacc0c27ccffe5723a3af967a28
alzheimeer <fogniebla@hotmail.com>
2020-02-06 19:46:26 -0500

    feature task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-'''that queries the Reddit API and returns the number of subscribers'''
+'''prints the titles of the first 10 hot posts listed for a given subreddit'''
+import requests
 
 
+def top_ten(subreddit):
+    ''' get top 10 titles'''
 
-
-if __name__ == '__main__':
-
+    url = 'http://reddit.com/r/{}/hot.json'
+    headers = {'User-agent': 'colombiandreamm'}
+    payload = {'limit': '10'}
+    response = requests.get(url.format(subreddit),
+                            headers=headers, params=payload)
+    if response.status_code == 200:
+        top10 = response.json()
+        for i in range(10):
+            print(top10['data']['children'][i]['data']['title'])
+    else:
+        return 0
