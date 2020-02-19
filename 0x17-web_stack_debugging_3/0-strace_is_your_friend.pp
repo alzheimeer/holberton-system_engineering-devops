f143fe0361bed2a18e3231fe911d31168ed29a3b
alzheimeer <fogniebla@hotmail.com>
2020-02-19 13:02:49 -0500

    fix task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
-# error php conf, a row says phpp and it must be php use sed -i to change this
-exec { 'change ext error':
-  command => '/bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php':
-}
-exec { 'restart server':
-  command => 'sudo service apache2 restart'
+# Fix
+
+exec { 'error conf php':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
