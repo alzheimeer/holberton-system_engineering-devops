852626b621214801dab3935d518b426190d21d54
Edgar <fogniebla@hotmail.com>
2020-09-20 20:19:16 -0500

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 '''that queries the Reddit API and returns the number of subscribers'''
+import requests
 
 
-
-
-if __name__ == '__main__':
-
+def count_words(subreddit, word_list):
+    """Returns the count of given words"""
+    try:
+        url = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+        header = {'User-agent': 'colombiandreamm'}
+        limit = {'limit': '10'}
+        resp = requests.get(url, headers=header,
+                            params=limit, allow_redirects=False).json()
+        key = resp.get('data').get('children')
+        for value in key:
+            print(value.get('data').get('title'))
+    except:
+        print(None)
